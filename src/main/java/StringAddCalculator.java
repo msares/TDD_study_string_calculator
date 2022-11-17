1d8c521fffe5dc5adc0f224b2f0431029d272ad8
송수정 <soojeong.song@datastreams.co.kr>
2022-11-17 13:15:55 +0900

    fix(StringAddCalculator): 문자열 덧셈 계산기 구현_리팩토링
    - README.md에 작성된 [두번째 구현] 리스트를 토대로 코드 수정

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,62 +1,56 @@
-
-/*
- * final 상수 적극 활용하기!
- *
- * 1. 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(예 : “” => 0, null => 0)
- * 2. 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.(예 : “1”)
- * 3. 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)
- * 4. 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다. (예 : “1,2:3” => 6)
- * 5. “//”와 “\n” 문자 사이에 커스텀 구분자를 지정할 수 있다. (예 : “//;\n1;2;3” => 6)
- * 6. 음수를 전달할 경우 RuntimeException 예외가 발생해야 한다. (예 : “-1,2,3”)
- * */
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
-    static String[] strSplitArr;
-    static String SPLIT_REGEX = "[,:]";
-    static final String CUSTOM_DELIMETER_REGEX = "//(.)\n(.*)";
-    static final String NUM_REGEX = "^[0-9]*$";
-    static final String ERR_MSG_MINUS = "숫자 이외의 값 또는 음수값은 계산이 불가능합니다.";
-    static Matcher matcher;
+    private static final String SPLIT_REGEX = "[,:]";
+    private static final String CUSTOM_DELIMETER_REGEX = "//(.)\n(.*)";
+    private static final String NUM_REGEX = "^[0-9]*$";
+    private static final String ERR_MSG_MINUS = "숫자 이외의 값 또는 음수값은 계산이 불가능합니다.";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(CUSTOM_DELIMETER_REGEX);
+    private static final int CUSTOM_DELIMITER_EXTRACT = 1;
+    private static final int CUSTOM_PLAIN_STRING_EXTRACT = 2;
 
-    public static int splitAndSum(String str) {
-        if (str == null || str.isEmpty()) {
+
+    public static int splitAndSum(String inputPlainString) {
+        String[] splitArr;
+        if (isThatNull(inputPlainString)) {
             return 0;
         }
-        splitCal(str);
-        return sumCal(strSplitArr);
+        splitArr = splitWorking(inputPlainString);
+        return sumCalculator(splitArr);
     }
 
-    private static void splitCal(String str) {
-        matcher = checkRegexPattern(str, CUSTOM_DELIMETER_REGEX);
+    private static boolean isThatNull(String inputPlainString) {
+        return inputPlainString == null || inputPlainString.isEmpty();
+    }
+
+
+    private static String[] splitWorking(String inputPlainString) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(inputPlainString);
         if (matcher.find()) {
-            SPLIT_REGEX = matcher.group(1);
-            str = matcher.group(2);
+            String customDelimiter = matcher.group(CUSTOM_DELIMITER_EXTRACT);
+            String customPlainString = matcher.group(CUSTOM_PLAIN_STRING_EXTRACT);
+            return customPlainString.split(customDelimiter);
         }
-        strSplitArr = str.split(SPLIT_REGEX);
+        return inputPlainString.split(SPLIT_REGEX);
     }
 
-    private static int sumCal(String[] strSplitArr) {
+    private static int sumCalculator(String[] splitArr) {
         int resultSum = 0;
-        for (String s : strSplitArr) {
-            checkNumRange(s);
-            resultSum += Integer.parseInt(s);
+        for (String s : splitArr) {
+            resultSum += checkNumRange(s);
         }
         return resultSum;
     }
 
-    private static Matcher checkRegexPattern(String str, String regexPattern) {
-        return Pattern.compile(regexPattern).matcher(str);
-    }
 
     // #. 숫자 이외의 값 또는 음수값일 경우 RuntimeException 발생
-    private static void checkNumRange(String strSplitArrElement) {
-        matcher = checkRegexPattern(strSplitArrElement, NUM_REGEX);
-        if (!matcher.find()) {
+    private static int checkNumRange(String strSplitArrElement) {
+        boolean isThatNum = Pattern.matches(NUM_REGEX, strSplitArrElement);
+        if (!isThatNum) {
             throw new RuntimeException(ERR_MSG_MINUS);
         }
+        return Integer.parseInt(strSplitArrElement);
     }
 }
 
